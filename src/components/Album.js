b4919e6de73346a45d92d463255e4547885c407c
Buck Pucker <egd1753@gmail.com>
2018-12-17 18:44:09 -0600

    completed checkpoint 8 work for player bar buttons

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import albumData from './../data/albums';
+import PlayerBar from './PlayerBar';
 
 class Album extends Component {
     constructor(props) {
@@ -72,6 +73,14 @@ class Album extends Component {
             }
         }
 
+        handlePrevClick() {
+            const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
+            const newIndex = Math.max(0, currentIndex - 1);
+            const newSong = this.state.album.songs[newIndex];
+            this.setSong(newSong);
+            this.play();
+        }
+
     render() {
         return (
             <section className="album">
@@ -92,15 +101,25 @@ class Album extends Component {
                     <tbody>  
                         {
                             this.state.album.songs.map( (song, index) => 
-                                <tr key={index} className="song" onClick={() => this.handleSongClick(song)} onMouseEnter={() => this.handleSetHoveredSong(song)} onMouseLeave={() => this.handleUnsetHoveredSong(song)}>  {this.handlePlayPauseButtons(song, index)} {song.title} {song.duration} </tr>
-
-                            )
-
-                            
-                        }
-                        
+                                <tr 
+                                    key={index} 
+                                    className="song" 
+                                    onClick={() => this.handleSongClick(song)} 
+                                    onMouseEnter={() => this.handleSetHoveredSong(song)} 
+                                    onMouseLeave={() => this.handleUnsetHoveredSong(song)}
+                                >
+                                {this.handlePlayPauseButtons(song, index)} {song.title} {song.duration} 
+                                </tr>
+                            ) 
+                        } 
                     </tbody>
                 </table>
+                <PlayerBar 
+                    isPlaying={this.state.isPlaying} 
+                    currentSong={this.state.currentSong} 
+                    handleSongClick={() => this.handleSongClick(this.state.currentSong)}
+                    handlePrevClick={() => this.handlePrevClick()}
+                />
             </section>
         );
     }
